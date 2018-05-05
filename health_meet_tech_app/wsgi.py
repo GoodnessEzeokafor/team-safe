52076e00915748ccf8ce5f7737c8165311198e24
bayoishola20 <bayoishola20@yahoo.com>
2018-05-05 18:42:07 +0100

    deployment config

--- health_meet_tech_app/wsgi.py
+++ health_meet_tech_app/wsgi.py
@@ -12,9 +12,10 @@ import os
 # For Heroku
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling # heroku
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "health_meet_tech_app.settings")
 
 application = get_wsgi_application()
-
+application = Cling(get_wsgi_application()) #heroku
 
